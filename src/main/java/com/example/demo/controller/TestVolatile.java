1d6d7b6c7d57cc613d51e2dc8b013f57809c590c
LongFei <changlf>
2021-10-26 17:36:38 +0800

    将第四次提交和第六次提交拉取合并

--- src/main/java/com/example/demo/controller/TestVolatile.java
+++ src/main/java/com/example/demo/controller/TestVolatile.java
@@ -50,6 +50,15 @@ public class TestVolatile {
         return RestResponse.success();
     }
 
+
+    public void test1111() {
+        String test1 = "test1第五次提交";
+    }
+
+    public void test11111() {
+        String test1 = "test1第六次提交";
+    }
+
     public static void main(String[] args) throws InterruptedException {
         final TestVolatile test = new TestVolatile();
         ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 10, 30, TimeUnit.SECONDS, new LinkedBlockingQueue<>());
