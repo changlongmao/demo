f89c5865b06e2c52275dfaa205d4346a7a569888
changlongmao <53187265+changlongmao@users.noreply.github.com>
2021-12-09 11:06:00 +0800

    Update ExecutorConfig.java

--- src/main/java/com/example/demo/config/ExecutorConfig.java
+++ src/main/java/com/example/demo/config/ExecutorConfig.java
@@ -36,7 +36,7 @@ public class ExecutorConfig {
         poolTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         // 等待所有任务结束后再关闭线程池
         poolTaskExecutor.setWaitForTasksToCompleteOnShutdown(true);
-        // 龙猫王帝第六代
+        // 龙猫王帝第七代
         return poolTaskExecutor;
     }
 }
